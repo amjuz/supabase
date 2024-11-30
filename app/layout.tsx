38f12662758dd782eb80a6072af6181cef3f1c75
Amjuz <137517288+amjuz@users.noreply.github.com>
2024-11-30 22:29:09 +0530

    Initialized: Drizzle ORM

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,5 @@
-import DeployButton from "@/components/deploy-button";
-import { EnvVarWarning } from "@/components/env-var-warning";
-import { ThemeSwitcher } from "@/components/theme-switcher";
-import { hasEnvVars } from "@/utils/supabase/check-env-vars";
 import { GeistSans } from "geist/font/sans";
 import { ThemeProvider } from "next-themes";
-import Link from "next/link";
 import "./globals.css";
 import Navbar from "@/components/navbar/navbar";
 import Footer from "@/components/footer";
